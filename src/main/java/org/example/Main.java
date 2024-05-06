01ebb6d1f791c6b6133626124a6b67e5572637bf
NancyD2017 <aa.alifanova@spbstu.ru>
2024-05-06 10:26:12 +0300

    part 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,27 +3,56 @@ package org.example;
 import org.apache.commons.math3.distribution.BinomialDistribution;
 import org.apache.commons.math3.random.RandomDataGenerator;
 
-import java.util.Arrays;
-import java.util.Random;
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        //System.out.println(second(100) + " " + second(1000) + " " + second(10000) + " " + second(100000) + " " + second(1000000));
-        //System.out.println(third(100) + " " + third(1000) + " " + third(10000) + " " + third(100000) + " " + third(1000000));
-        //System.out.println("Аналитическое решение - 0.2113");
-        // int n = 120;
-        //int k = 50;
-        //int m = 10;
-        //System.out.println("Аналитическое решение " + analiticFourth(n,k,m) + "\nпри n = " + n + " k = " + k + " m = " + m);
-        //System.out.println(fourth(100,n,k,m) + "\n" + fourth(1000,n,k,m) + "\n" + fourth(10000,n,k,m) + "\n" +
-        //fourth(100000,n,k,m) + "\n" + fourth(1000000,n,k,m));
-        //System.out.println(fifth(100) + " " + fifth(1000) + " " + fifth(10000) + " " + fifth(100000) + " " + fifth(1000000));
-        //System.out.println("Решение аналитически: 2 группа\n" + seventh(100) + " " + seventh(1000) + " " + seventh(10000) + " " + seventh(100000) + " " + seventh(1000000));
-        //System.out.printf("%.2f %.3f %.4f %.5f %.6f\nРешение аналитически: 0.665510", eighth(100), eighth(1000), eighth(10000), eighth(100000), eighth(1000000));
-        //System.out.printf("%-10s %-10s%n" + nineth(100) + nineth(1000) + nineth(10000) + nineth(100000) + nineth(1000000) + "\nРешение аналитически:\na)0.0485735 б)0.205099", "a)", "б)");
-        //!!!System.out.println(tenth(100,0.4,50) + " " + tenth(1000,0.4,50) +" " + tenth(10000,0.4,50)+ " " + tenth(100000,0.4,50)+ " " + tenth(1000000,0.4,50));
-        System.out.println(eleventh(100) + " " + eleventh(1000) + " " + eleventh(10000) + " " + eleventh(100000) + " " + eleventh(1000000));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Какую задачу Вы хотите посмотреть (введите число от 1 до 15)?");
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 2 ->
+                System.out.println(second(100) + " " + second(1000) + " " + second(10000) + " " + second(100000) + " " + second(1000000));
+            case 3 ->
+                System.out.println(third(100) + " " + third(1000) + " " + third(10000) + " " + third(100000) + " " + third(1000000) + "\nАналитическое решение - 0.2113");
+            case 4 -> {
+                int n = 120;
+                int k = 50;
+                int m = 10;
+                System.out.println("Аналитическое решение " + analiticFourth(n, k, m) + "\nпри n = " + n + " k = " + k + " m = " + m);
+                System.out.println(fourth(100, n, k, m) + "\n" + fourth(1000, n, k, m) + "\n" + fourth(10000, n, k, m) + "\n" + fourth(100000, n, k, m) + "\n" + fourth(1000000, n, k, m));
+            }
+            case 5 ->
+            System.out.println(fifth(100) + " " + fifth(1000) + " " + fifth(10000) + " " + fifth(100000) + " " + fifth(1000000));
+            case 6 ->
+                    System.out.println(sixth(100) + " " + sixth(1000) + " " + sixth(10000) + " " + sixth(100000) + " " + sixth(1000000));
+            case 7 ->
+            System.out.println("Решение аналитически: 2 группа\n" + seventh(100) + " " + seventh(1000) + " " + seventh(10000) + " " + seventh(100000) + " " + seventh(1000000));
+            case 8 ->
+                    System.out.printf("%.2f %.3f %.4f %.5f %.6f\nРешение аналитически: 0.665510", eighth(100), eighth(1000), eighth(10000), eighth(100000), eighth(1000000));
+            case 9 ->
+                    System.out.printf("%-10s %-10s%n" + nineth(100) + nineth(1000) + nineth(10000) + nineth(100000) + nineth(1000000) + "\nРешение аналитически:\na)0.0485735 б)0.205099", "a)", "б)");
+            case 10 -> {
+                tenth(1000, 0.4, 50);
+                //tenth(1000, 0.3, 50);
+                //tenth(1000, 0.2, 50);
+                //tenth(1000, 0.1, 50);
+            }
+            case 11 ->
+                    System.out.println(eleventh(100) + " " + eleventh(1000) + " " + eleventh(10000) + " " + eleventh(100000) + " " + eleventh(1000000) + "\nАналитическое решение: 2");
+            case 12 -> twelveth();
+            case 13 -> thirteenth(5);
+            case 14 ->
+                    System.out.println(fourteenth(100) + " " + fourteenth(1000) + " " + fourteenth(10000) + " " + fourteenth(100000) + " " + fourteenth(1000000) + "\nАналитическое решение: 0.781730");
+            case 15 -> {
+                fifteenth(-1);
+                fifteenth(0);
+                fifteenth(1);
+                fifteenth(5);
+            }
+            default -> System.out.println("Такую задачу пока не решили");
+        }
     }
 
     public static double second(int experiments) {
@@ -187,55 +216,113 @@ public class Main {
         return String.format("%.6f   %.6f\n", successfulA / experiments, successfulB / experiments);
         //Решение аналитически: a)0.0485735    б)0.205099
     }
-    public static double tenth(int experiments, double p, int r){
-        double successful = 0.0;
-        int newR = r;
-        for (int i = 0; i <experiments; i++){
-            Random random = new Random();
-            double trying = random.nextDouble();
-            if (trying <= p) {
-                newR--;
-                successful++;
+    public static double tenth(int experiments, double p, int r) {
+        for (int j = 0; j < 1000; j++) {
+            double successful = 0.0;
+            int newR = r;
+            for (int i = 0; i < experiments; i++) {
+                Random random = new Random();
+                double trying = random.nextDouble();
+                if (trying <= p) {
+                    newR--;
+                    successful++;
+                }
+                if (newR == 0) {
+                    System.out.println(i);
+                    break;
+                }
             }
-            if (newR == 0) return successful/i;
+            if (newR != 0) System.out.println(experiments);
         }
-        return successful/experiments;
+        return 0;
     }
 
     public static int eleventh(int experiments) {
-        int[] weightsSet1 = {1, 1, 2, 2, 1, 2, 2, 3, 3, 1};
-        int[] weightsSet2 = {1, 1, 1, 1, 2, 2, 2, 3, 3, 1};
-        int[] weightsSet3 = {1, 1, 2, 3, 1, 2, 2, 3, 4, 1};
+        int[][] weightsSet = {{1, 1, 2, 2, 1, 2, 2, 3, 3, 1},
+                {1, 1, 1, 1, 2, 2, 2, 3, 3, 1},
+                {1, 1, 2, 3, 1, 2, 2, 3, 4, 1}};
+        int[] weightCount = {0, 0, 0};
         for (int i = 0; i < experiments; i++) {
-            int weightToWeigh = (int) (Math.random() * 10) + 1;
-            int[] dp1 = new int[weightToWeigh + 1];
-            int[] dp2 = new int[weightToWeigh + 1];
-            int[] dp3 = new int[weightToWeigh + 1];
-            Arrays.fill(dp1, Integer.MAX_VALUE);
-            Arrays.fill(dp2, Integer.MAX_VALUE);
-            Arrays.fill(dp3, Integer.MAX_VALUE);
-            dp1[0] = 0;
-            dp2[0] = 0;
-            dp3[0] = 0;
-            for (int i1 = 0; i1 < 5; i1++) {
-                for (int j = weightsSet1[i1]; j <= weightToWeigh; j++) {
-                    dp1[j] = Math.min(dp1[j], dp1[j - weightsSet1[i1]] + 1);
-                }
+            int weightToWeigh = (int) (Math.random() * 10);
+            for (int j = 0; j < 3; j++){
+                weightCount[j] += weightsSet[j][weightToWeigh];
             }
-            for (int i2 = 0; i2 < 5; i2++) {
-                for (int j = weightsSet2[i2]; j <= weightToWeigh; j++) {
-                    dp2[j] = Math.min(dp2[j], dp2[j - weightsSet2[i2]] + 1);
-                }
-            }
-            for (int i3 = 0; i3 < 5; i3++) {
-                for (int j = weightsSet3[i3]; j <= weightToWeigh; j++) {
-                    dp3[j] = Math.min(dp3[j], dp3[j - weightsSet3[i3]] + 1);
-                }
-            }
-            System.out.println(Arrays.toString(dp1));
-            System.out.println(Arrays.toString(dp2));
-            System.out.println(Arrays.toString(dp3));
         }
-        return 1;
+        if (weightCount[0] < weightCount[1] && weightCount[0] < weightCount[2]) return 1;
+        if (weightCount[1] < weightCount[0] && weightCount[1] < weightCount[2]) return 2;
+        return 3;
+        //Аналитическое решение: 2
+    }
+    public static void twelveth() {
+        List<Double> statisticsA = new ArrayList<>();
+        List<Double> statisticsB = new ArrayList<>();
+        for (int j = 0; j < 1000; j++) {
+            Random random = new Random();
+            double x = random.nextDouble() * 10 - 5;
+            double solutionA = 0;
+            if (x >= 0 && x <= 1) solutionA = x;
+            else if (x > 1) solutionA = 1;
+            Double solutionB = Math.exp(-x * x / 2) / Math.sqrt(2 * Math.PI);
+            statisticsA.add(solutionA);
+            statisticsB.add(solutionB);
+        }
+        for (Double solution: statisticsA) System.out.println(solution);
+        System.out.println("\n\n");
+        for (Double solution: statisticsB) System.out.println(solution);
+    }
+    public static void thirteenth(double a) {
+        List<Double> statistics = new ArrayList<>();
+        for (int j = 0; j < 1000; j++) {
+            Random random = new Random();
+            double x = random.nextDouble() * 40 - 20;
+            double solution = a/(Math.PI * (x * x + a * a));
+            statistics.add(solution);
+        }
+        for (Double solution: statistics) System.out.println(solution);
+    }
+    public static double fourteenth(int experiments) {
+        double sigma = 20.0;
+        double mu1 = 5.0;
+        double mu2 = -5.0;
+        double goodExperiments = 0;
+        for (int i = 0; i < experiments; i++) {
+            Random random1 = new Random();
+            Random random2 = new Random();
+            double x1 = mu1 + sigma * Math.sqrt(-2.0 * Math.log(random1.nextDouble())) * Math.cos(2.0 * Math.PI * random1.nextDouble());
+            double x2 = mu2 + sigma * Math.sqrt(-2.0 * Math.log(random2.nextDouble())) * Math.cos(2.0 * Math.PI * random2.nextDouble());
+            if (Math.abs(x1) <= 15 || Math.abs(x2) <= 15) goodExperiments++;
+        }
+        return goodExperiments/experiments;
+        //Аналитическое решение: 0.781730
+    }
+    public static long factorial(int n) {
+        if (n <= 0) {
+            return 1;
+        }
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+    public static void fifteenth(double m0) {
+        List<Double> statistics = new ArrayList<>();
+        int min = 0;
+        int max = 5;
+        if (m0 == 1) {
+            min = -1;
+            max = 7;
+        }
+        else if (m0 == 5) {
+            min = -2;
+            max = 9;
+        }
+        for (int j = 0; j < 1000; j++) {
+            Random random = new Random();
+            double a = random.nextDouble() * max + min;
+            double solution = (2 * Math.exp(-2 * a) * Math.pow(2*a,m0+1))/(factorial((int) (m0+1)));
+            statistics.add(solution);
+        }
+        for (Double solution: statistics) System.out.println(solution);
     }
 }
